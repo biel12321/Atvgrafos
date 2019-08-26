7dab778f792101c5d477a9e04f9b080ef58254e4
biel12321 <gabriellw.grb@gmail.com>
2019-08-26 17:08:34 -0300

    Adicionado function para resolução do problema

--- src/grafos/Grafos.java
+++ src/grafos/Grafos.java
@@ -56,13 +56,72 @@ public class Grafos {
 
     //Metodo de Resolução da primeira opção
     private static void nao_ordenado() {
-        
+        if(valoradoOuNaoValorado()==1){
+            //Valorado
+            listadeArestas(true);
+            listadeAdjacentes(true);
+            matrizAdjacente(true);
+            matrizInsidencia(true);
+        } else {
+            //Não_Valorado
+            listadeArestas(false);
+            listadeAdjacentes(false);
+            matrizAdjacente(false);
+            matrizInsidencia(false);
+        }
     }
     
     //Metodo de Resolução da segunda opção
     private static void ordenado() {
-        
+        if(valoradoOuNaoValorado()==1){
+            //Valorado
+            
+        } else {
+            //Não_Valorado
+            
+        }
     }
     
-    
+    public static int valoradoOuNaoValorado(){
+        int orientado = 0;
+        orientado = Integer.parseInt(JOptionPane.showInputDialog(null, "Este grafo sera valorado?"
+                + "\n 0 - Não\n 1 - Sim"));
+        if(orientado!=0 && orientado!= 1){
+            JOptionPane.showMessageDialog(null, "Opção invalida");
+            valoradoOuNaoValorado();
+        } else {
+            return orientado;
+        }
+        return 0;
+    }
+
+    private static void listadeArestas(boolean b) {
+        if(b){
+            int opcao = 0;
+            boolean flag;
+            do{
+                double valor = Double.parseDouble(JOptionPane.showInputDialog("Valor do grafo:"));
+                //Codigo...
+                
+                
+                
+                opcao = JOptionPane.showConfirmDialog(null, "Escolha sim ou não", "Sim ou não?", JOptionPane.YES_NO_OPTION);
+                flag = opcao == JOptionPane.YES_OPTION;
+            }while( flag );
+        } else {
+            
+        }
+    }
+
+    private static void listadeAdjacentes(boolean b) {
+        
+    }
+
+    private static void matrizAdjacente(boolean b) {
+        
+    }
+
+    private static void matrizInsidencia(boolean b) {
+        
+    }
 }
